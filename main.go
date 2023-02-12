e1d97b04d14df317ca093e7339a80a520db0a80c
Kininaru <shiftregister233@outlook.com>
2023-02-12 23:32:07 +0800

    feat: record pid

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"topaz/database"
 	_ "topaz/routers"
+	"topaz/utils"
 
 	"github.com/beego/beego"
 )
 
 func main() {
 	os.Mkdir("data", os.ModePerm)
+	utils.OverrideFile(fmt.Sprintf("%d", os.Getpid()), "data/pid")
+
 	database.Init()
 
 	beego.BConfig.WebConfig.Session.SessionOn = true
